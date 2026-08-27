f8da646285adba60bdd6d4513e75b433b66df1ef
lokendra Kumar <lokendrakumar4812gmail.com>
2026-08-27 23:35:53 +0530

    Add latest logical operator examples
    Publish the updated OR example and new or1 C source file.

---
+++ logicalopretors/logicaloretoror/OR.c
@@ -0,0 +1,3 @@
+// in or  logical opretors in one value  is  true and  others  are  non  zeros and
+// OR -||   this  sybole  use 
+
---
+++ logicalopretors/logicaloretoror/or1.c
@@ -0,0 +1,13 @@
+/*#include <stdio.h>
+ void main() {
+     int x =10;
+      int  y= 20;
+       printf("%d" , x||y);
+ }*/
+ # include<stdio.h>
+  void main() {
+     int  x = 0;
+      int   y = 9;
+       printf("%d",  x||y);
+
+  }
